deb0df734396c7c267b715d7095a7264365e1198
Blair McMillan <blair@mcmillan.id.au>
2014-02-14 15:55:16 +1000

    Adding countdown timer shortcuts

--- QLab CasparCG ATEM Bridge/AppDelegate.h
+++ QLab CasparCG ATEM Bridge/AppDelegate.h
@@ -18,7 +18,18 @@
 @property (weak) IBOutlet NSTextField *ipAddressTextField;
 @property (weak) IBOutlet NSTextField *portTextField;
 @property (weak) IBOutlet NSButton *connectButton;
+@property (weak) IBOutlet NSTextField *commandTextLabel;
 @property (weak) IBOutlet NSTextField *commandTextField;
 @property (weak) IBOutlet NSButton *sendCommandButton;
+@property (weak) IBOutlet NSButton *countdownTimerButton;
+@property (weak) IBOutlet NSButton *hideTimerButton;
+@property (weak) IBOutlet NSTextField *minutesLabel;
+@property (weak) IBOutlet NSTextField *minutesTextField;
+@property (weak) IBOutlet NSTextField *secondsLabel;
+@property (weak) IBOutlet NSTextField *secondsTextField;
+@property (weak) IBOutlet NSTextField *layerLabel;
+@property (weak) IBOutlet NSTextField *layerTextField;
+@property (weak) IBOutlet NSTextField *templateLabel;
+@property (weak) IBOutlet NSTextField *templateTextField;
 
 @end
--- QLab CasparCG ATEM Bridge/AppDelegate.m
+++ QLab CasparCG ATEM Bridge/AppDelegate.m
@@ -17,30 +17,69 @@
 }
 - (IBAction)handleConnectButtonClick:(id)sender {
     
-    NSLog(@"Ready to create socket");
-    
     asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     
     NSError* error = Nil;
     if (![asyncSocket connectToHost:[self.ipAddressTextField stringValue] onPort:[[self.portTextField stringValue] intValue] withTimeout:10 error:&error])
     {
-        NSLog(@"Failed to create socket");
+        NSLog(@"Failed to connect to CasparCG");
         self.connectButton.title = @"Connect";
+        [self.commandTextLabel setHidden:true];
         [self.commandTextField setHidden:true];
         [self.sendCommandButton setHidden:true];
+        [self.countdownTimerButton setHidden:true];
+        [self.hideTimerButton setHidden:true];
+        [self.minutesLabel setHidden:true];
+        [self.minutesTextField setHidden:true];
+        [self.secondsLabel setHidden:true];
+        [self.secondsTextField setHidden:true];
+        [self.layerLabel setHidden:true];
+        [self.layerTextField setHidden:true];
+        [self.templateLabel setHidden:true];
+        [self.templateTextField setHidden:true];
     }
     
 }
+
+- (void)socket:(GCDAsyncSocket *)sender didConnectToHost:(NSString *)host port:(UInt16)port
+{
+    NSLog(@"Connected to CasparCG.");
+    self.connectButton.title = @"Connected";
+    [self.commandTextLabel setHidden:false];
+    [self.commandTextField setHidden:false];
+    [self.sendCommandButton setHidden:false];
+    [self.countdownTimerButton setHidden:false];
+    [self.hideTimerButton setHidden:false];
+    [self.minutesLabel setHidden:false];
+    [self.minutesTextField setHidden:false];
+    [self.secondsLabel setHidden:false];
+    [self.secondsTextField setHidden:false];
+    [self.layerLabel setHidden:false];
+    [self.layerTextField setHidden:false];
+    [self.templateLabel setHidden:false];
+    [self.templateTextField setHidden:false];
+}
+
 - (IBAction)sendCommandButton:(id)sender {
     NSString *command = [[self.commandTextField stringValue] stringByAppendingString:@"\r\n"];
     [asyncSocket writeData:[command dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES] withTimeout:-1 tag:1];
 }
 
-- (void)socket:(GCDAsyncSocket *)sender didConnectToHost:(NSString *)host port:(UInt16)port
-{
-    NSLog(@"Cool, I'm connected! That was easy.");
-    self.connectButton.title = @"Connected";
-    [self.commandTextField setHidden:false];
-    [self.sendCommandButton setHidden:false];
+- (IBAction)hideTimerButton:(id)sender {
+    NSString *layer = [self.layerTextField stringValue];
+    NSString *command = [NSString stringWithFormat:@"STOP %@ \r\n", layer];
+    [asyncSocket writeData:[command dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES] withTimeout:-1 tag:1];
+}
+
+- (IBAction)countdownTimerButton:(id)sender {
+    NSString *layer = [self.layerTextField stringValue];
+    NSString *template = [self.templateTextField stringValue];
+    NSString *minutes = [self.minutesTextField stringValue];
+    NSString *seconds = [self.secondsTextField stringValue];
+    NSString *formatted = [NSString stringWithFormat:@"%02d:%02d", [minutes intValue], [seconds intValue]];
+    NSString *command = [NSString stringWithFormat:@"CG %@ ADD 0 \"%@\" 1 \"<templateData><componentData id=\\\"timer_txt\\\"><data id=\\\"text\\\" value=\\\"%@\\\"></data> </componentData><componentData id=\\\"minn\\\"><data id=\\\"text\\\" value=\\\"%@\\\"></data></componentData><componentData id=\\\"secc\\\"><data id=\\\"text\\\" value=\\\"%@\\\"></data></componentData><componentData id=\\\"Dirr\\\"><data id=\\\"text\\\" value=\\\"down\\\"></data></componentData></templateData>\"\r\n", layer, template, formatted, minutes, seconds];
+    [asyncSocket writeData:[command dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES] withTimeout:-1 tag:1];
+    command = [NSString stringWithFormat:@"CG %@ INVOKE 0 \"TimeStart\"\r\n", layer];
+    [asyncSocket writeData:[command dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES] withTimeout:-1 tag:1];
 }
 @end
